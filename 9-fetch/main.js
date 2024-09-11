584601722323fee4b81a7892d8f1cad4c43d8b75
Alberto Caminos <albeerc@gmail.com>
2024-09-11 09:41:52 -0300

    entrega trabajo fetch-api

--- 9-fetch/main.js
+++ 9-fetch/main.js
@@ -7,10 +7,11 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=59a8b9ea3a6d0f0d1d79
             container.innerHTML += `
                 <div>
                     <img src="https://image.tmdb.org/t/p/w185${movie.poster_path}" alt="${movie.title}">
-                    <p>${movie.title}</p>
+                    <h3>${movie.title}</h3>
                     <p>${movie.vote_average.toFixed(1)} / 10</p>
                     <p>${movie.release_date.substring(0, 4)}</p>
                 </div>
             `
         })
     })
+    .catch((error) => console.log(error))

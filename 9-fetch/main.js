c9d69aa3f651fd6750ef1c8e5c7f2051b2644c67
Alberto Caminos <albeerc@gmail.com>
2024-09-12 10:05:38 -0300

    cree card con createElement y agregue header

--- 9-fetch/main.js
+++ 9-fetch/main.js
@@ -4,14 +4,16 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=59a8b9ea3a6d0f0d1d79
     .then((response) => response.json())
     .then((data) => {
         data.results.forEach((movie) => {
-            container.innerHTML += `
-                <div>
-                    <img src="https://image.tmdb.org/t/p/w185${movie.poster_path}" alt="${movie.title}">
-                    <h3>${movie.title}</h3>
-                    <p>${movie.vote_average.toFixed(1)} / 10</p>
-                    <p>${movie.release_date.substring(0, 4)}</p>
-                </div>
+            const div = document.createElement("div")
+            div.classList.add("movie-card")
+            div.innerHTML = `
+                <img src="https://image.tmdb.org/t/p/w185${movie.poster_path}" alt="${movie.title}">
+                <h3>${movie.title}</h3>
+                <p>${movie.vote_average.toFixed(1)} / 10</p>
+                <p>${movie.release_date.substring(0, 4)}</p>
             `
+            
+            container.appendChild(div)
         })
     })
     .catch((error) => console.log(error))
